b20b70ed68e723ef77aaea6780e5083a59589472
Nethelbert <gilblasse@gmail.com>
2019-11-09 19:41:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,13 @@
 class Book
-  attr_accessible :title
+  attr_accessible :title :author_name :page_count :genre
   
   def initialize (title)
     @title = title
+    
   end
   
+  def turn_page
+    
+  end
   
 end
